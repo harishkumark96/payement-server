fd60cf266c7f718b06816b2a9faad20ea29117cf
harishkumark96 <harishkumark05@gmail.com>
2022-10-25 21:09:37 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,137 @@
+const express =  require('express');
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config()
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors({origin:true, credentials:true}));
+
+const stripe = require('stripe')(process.env.key);
+
+app.post('/checkout',async (req,res,next)=>{
+  try {
+    const session = await stripe.checkout.sessions.create({
+    // payment_method_types: ['card'],
+    shipping_address_collection: {
+    allowed_countries: ['IN'],
+    },
+        shipping_options: [
+        {
+            shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+                amount: 0,
+                currency: 'inr',
+            },
+            display_name: 'Free shipping',
+            // Delivers between 5-7 business days
+            delivery_estimate: {
+                minimum: {
+                unit: 'business_day',
+                value: 5,
+                },
+                maximum: {
+                unit: 'business_day',
+                value: 7,
+                },
+            }
+            }
+        },
+        {
+            shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+                amount: 1500,
+                currency: 'inr',
+            },
+            display_name: 'Next day air',
+            // Delivers in exactly 1 business day
+            delivery_estimate: {
+                minimum: {
+                unit: 'business_day',
+                value: 1,
+                },
+                maximum: {
+                unit: 'business_day',
+                value: 1,
+                },
+            }
+            }
+        },
+        ],
+       line_items:  req.body.items.map((item) => ({
+        price_data: {
+          currency: 'inr',
+          product_data: {
+            name: item.name,
+            images: [item.product]
+          },
+          unit_amount: item.price * 100 * 82,
+        },
+        quantity: item.quantity,
+      })),
+       mode: "payment",
+       success_url: "http://localhost:3000/success.html",
+       cancel_url: "http://localhost:3000/cancel.html",
+    });
+
+    res.status(200).json(session);
+} catch (error) {
+    next(error);
+}
+})
+
+app.get('success',(req,res)=>{
+  res.render('success.html')
+})
+app.listen(3000,()=>{
+  console.log(`server is connected to port 3000`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// try{
+//   const session = await stripe.checkout.sessions.create({
+
+//    line_items : req.body.items.map((item) =>( {
+//      price_data: {
+//        currency: 'INR',
+//        product_data: {
+//          name: item.name,
+//          images:[item.product]
+//        },
+//        unit_amount: item.price *100,
+//      },
+//      quantity: item.quantity,
+//    })),
+//    mode: 'payment',
+//    success_url: "http://localhost:3000/success.html",
+//         cancel_url: "http://localhost:3000/cancel.html",
+//   });
+//   res.status(200).json(session)
+// }catch(error){
+// next(error)
+// }
